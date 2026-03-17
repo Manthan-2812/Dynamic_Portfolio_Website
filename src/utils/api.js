7055097e198ad7c81c65b6b21a67184c3929dd24
Manthan-2812 <manthanparekh9d@gmail.com>
2026-03-17 15:51:11 +0530

    Use VITE_API_URL env variable for API base URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://portfolio-backend-nbfk.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(config => {
